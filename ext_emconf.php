a476808fdbc0767b47a43ff050b6cc9162d51a4b
Cyril Wolfangel <cyril.wolfangel@gmail.com>
2025-08-01 08:57:47 +0200

    upgrade somme dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.3.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
+            'typo3' => '12.4.0-13.99.99',
             'php' => '8.1.0-8.99.99'
         ],
         'conflicts' => [],
